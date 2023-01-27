d20f3fb20336efbc70b534dce922f90149a08441
Tarun Kumar <tarun.kumar@infostride.com>
2023-01-27 14:28:33 +0530

    adding custom taxonomy updated page

--- wp-content/themes/barrier/taxonomy-product_cat.php
+++ wp-content/themes/barrier/taxonomy-product_cat.php
@@ -31,10 +31,10 @@ $taxonomy_img = get_taxonomy_image( $taxonomy->term_taxonomy_id );
                <div class="col-md-8 ps-4  d-flex align-items-center">
                   <div class="row">
                      <div class="col-md-10">
-                        <?php the_field('category_title'); ?>
+                        <?php the_field('category_title',$taxonomy); ?>
                         <p class="product-text"><?php echo $taxonomy->description; ?></p>
-                        <h3 class="m-0"><?php the_field('category_title_copy'); ?></h3>
-                        <?php the_field('features'); ?>
+                        <h3 class="m-0"><?php the_field('category_title_copy',$taxonomy); ?></h3>
+                        <?php the_field('features',$taxonomy); ?>
                         <div class="mt-4"> <button type="button" class="product-button px-5 btn btn-outline-primary">See
                               all Drum Liners</button></div>
                      </div>
@@ -52,100 +52,184 @@ $taxonomy_img = get_taxonomy_image( $taxonomy->term_taxonomy_id );
                <div class="col-md-3 mb-3">
                   <h3>Apply by Filters</h3>
                   <div class="filter">
-                     <div class="accordion accordion-flush" id="accordionFlushExample">
-                        <div class="accordion-item mb-2">
-                           <h2 class="accordion-header" id="flush-headingOne">
-                              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                                 data-bs-target="#flush-collapseOne" aria-expanded="false"
-                                 aria-controls="flush-collapseOne">
-                                 Thickness
-                              </button>
-                           </h2>
-                           <div id="flush-collapseOne" class="accordion-collapse collapse"
-                              aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
-                              <div class="accordion-body">
-                                 <div class="d-flex justify-content-between p-0">
-                                    <div class="p-2"><input class="form-check-input" type="checkbox" value=""
-                                          id="flexCheckDefault">
-                                       <span class="mx-4">Fl</span>
-                                    </div>
-                                    <div class="ml-auto p-2">(2)</div>
-                                 </div>
-                              </div>
-                           </div>
-                        </div>
-                        <div class="accordion-item mb-2">
-                           <h2 class="accordion-header" id="flush-headingTwo">
-                              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                                 data-bs-target="#flush-collapseTwo" aria-expanded="false"
-                                 aria-controls="flush-collapseTwo">
-                                 Width
-                              </button>
-                           </h2>
-                           <div id="flush-collapseTwo" class="accordion-collapse collapse"
-                              aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
-                              <div class="accordion-body">
-                                 <div class="d-flex justify-content-between p-0">
-                                    <div class="p-2"><input class="form-check-input" type="checkbox" value=""
-                                          id="flexCheckDefault">
-                                       <span class="mx-4">Fl</span>
-                                    </div>
 
-                                    <div class="ml-auto p-2">(2)</div>
-                                 </div>
-                              </div>
-                           </div>
-                        </div>
-                        <div class="accordion-item mb-2">
-                           <h2 class="accordion-header" id="flush-headingThree">
-                              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                                 data-bs-target="#flush-collapseThree" aria-expanded="false"
-                                 aria-controls="flush-collapseThree">
-                                 Length
-                              </button>
-                           </h2>
-                           <div id="flush-collapseThree" class="accordion-collapse collapse"
-                              aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
-                              <div class="accordion-body">
-                                 <div class="d-flex justify-content-between p-0">
-                                    <div class="p-2"><input class="form-check-input" type="checkbox" value=""
-                                          id="flexCheckDefault">
-                                       <span class="mx-4">Fl</span>
-                                    </div>
-
-                                    <div class="ml-auto p-2">(2)</div>
-                                 </div>
-                              </div>
-                           </div>
-                        </div>
-                        <div class="accordion-item mb-2">
-                           <h2 class="accordion-header" id="flush-headingFour">
-                              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                                 data-bs-target="#flush-collapseFour" aria-expanded="false"
-                                 aria-controls="flush-collapseThree">
-                                 Color
-                              </button>
-                           </h2>
-                           <div id="flush-collapseFour" class="accordion-collapse collapse"
-                              aria-labelledby="flush-headingFour" data-bs-parent="#accordionFlushExample">
-                              <div class="accordion-body">
-                                 <div class="d-flex justify-content-between p-0">
-                                    <div class="p-2"><input class="form-check-input" type="checkbox" value=""
-                                          id="flexCheckDefault">
-                                       <span class="mx-4">Fl</span>
-                                    </div>
-
-                                    <div class="ml-auto p-2">(2)</div>
-                                 </div>
-                              </div>
-                           </div>
-                        </div>
-                     </div>
+                     <?php echo do_shortcode('[fe_widget id="150" show_selected="no" show_count="yes"]'); ?>
                   </div>
                </div>
-               <div class="col-md-9 mb-3">
+               <div class="col-md-9 mb-3 ajax_posts">
                   <?php
-                  $product_child_terms =  get_terms(
+                     $unique_terms = [];
+                     $i = 0;
+                     if(have_posts()){
+                        while( have_posts()){
+                           the_post();
+
+                           if ( empty( $product ) || ! $product->is_visible() ) {
+                              return;
+                           }
+                           $terms = get_the_terms(get_the_ID(), 'product_cat');
+                           foreach ( $terms as $key => $val ){
+                              if($val->parent > 0){
+                                 $unique_terms[$i] = $val;
+                                 // echo '<div class="parent '.$val->slug.'">' . $val->name . '</div>';
+                              }
+                           }
+                           // wc_get_template_part( 'content', 'product' );
+                           $i++;
+                        }
+                     }
+
+                     $duplicate_keys = array();
+                     $tmp = array();       
+
+                     foreach ($unique_terms as $key => $val){
+                        if (is_object($val))
+                           $val = (array)$val;
+
+                        if (!in_array($val, $tmp))
+                           $tmp[] = $val;
+                        else
+                           $duplicate_keys[] = $key;
+                     }
+
+                     foreach ($duplicate_keys as $key)
+                        unset($unique_terms[$key]);
+
+
+                     foreach($unique_terms as $key => $value){
+                        echo "<h3>".$value->name."</h3>";
+
+                        $args = array(
+                                 'post_status' => 'publish',
+                                 'tax_query' => array(
+                                    array(
+                                       'taxonomy' => 'product_cat',
+                                       'field'    => 'term_id',
+                                       'terms'     =>  $value->term_id, // When you have more term_id's seperate them by komma.
+                                       'operator'  => 'IN'
+                                    )
+                                 )
+                              );
+                        $products = new wp_query($args);
+
+                        if($products->have_posts()) :
+                           $newloopcount = 1;
+                           while($products->have_posts()) : $products->the_post(); 
+                              $loopcount = $newloopcount.generateRandomString();
+                              $price = get_post_meta( get_the_ID(), '_price', true );
+                              $length = get_post_meta( get_the_ID(), '_length', true );
+                              $width = get_post_meta( get_the_ID(), '_width', true );
+                              $height = get_post_meta( get_the_ID(), '_height', true );
+                              $sku = get_post_meta( get_the_ID(), '_sku', true );
+                              ?>
+
+                              <div class="wrapper">
+                                 <div class="accordion" id="accordionExample">
+                                    <div class="accordion-item mb-2 border-0">
+                                       <h2 class="accordion-header d-none d-sm-block" id="heading<?=$loopcount?>">
+                                          <button class="accordion-button  collapsed p-2" type="button" data-bs-toggle="collapse"
+                                             data-bs-target="#collapse<?=$loopcount?>" aria-expanded="true" aria-controls="collapse<?=$loopcount?>">
+                                             <div class="table-data pt-2">
+                                                <dl class="pe-3">
+                                                   <dt style="display: inline-block;">Stock:</dt>
+                                                   <dd style="display: inline-block;margin-left: 2px;" class="stock-text"> <?=$sku?></dd>
+                                                </dl>
+                                                <dl class="pe-3">
+                                                   <dt style="display: inline-block;">#WxL: </dt>
+                                                   <dd style="display: inline-block;"> <?=$width?>" x <?=$length?>"</dd>
+                                                </dl>
+                                                <dl class="pe-3">
+                                                   <dt style="display: inline-block;">Thickness:</dt>
+                                                   <dd style="display: inline-block;"> <?=$height?> Mil</dd>
+                                                </dl>
+                                                <dl class="pe-3">
+                                                   <dt style="display: inline-block;">Color:</dt>
+                                                   <dd style="display: inline-block;"> Clear</dd>
+                                                </dl>
+                                                <dl class="pe-3">
+                                                   <dt style="display: inline-block;">Qty/Roll:</dt>
+                                                   <dd style="display: inline-block;"> 300 / Roll</dd>
+                                                </dl>
+                                                <dl class="pe-3">
+                                                   <dt style="display: inline-block;">Price:</dt>
+                                                   <dd style="display: inline-block;"> As low as $<?=$price?></dd>
+                                                </dl>
+                                             </div>
+                                          </button>
+                                       </h2>
+                                       <div id="collapse<?=$loopcount?>" class="accordion-collapse collapse" aria-labelledby="heading<?=$loopcount?>"
+                                          data-bs-parent="#accordionExample">
+                                          <div class="accordion-body">
+                                             <div class="container">
+                                                <div class="col-md-12 col-12 row d-flex align-items-center">
+                                                   <div class="col-md-8 col-12">
+                                                      <div class="row col-md-12 col-12 d-flex align-items-center">
+                                                         <div class="col-md-4 col-5">
+                                                            <img src="<?php echo get_the_post_thumbnail_url(); ?>" class="img-fluid" alt="">
+                                                         </div>
+                                                         <div class="col-md-8 col-7">
+                                                            <span><?php the_title(); ?>
+                                                               Stock #: <?=$sku?></span>
+                                                            <div class="view-text py-4"><a href="<?php echo get_permalink(); ?>">View Details</a></div>
+                                                         </div>
+                                                      </div>
+                                                   </div>
+                                                   <div class="col-md-4">
+                                                      <div class="d-none d-sm-block">
+                                                         <dl class="pe-3">
+                                                            <dt style="display: inline-block;">Price </dt>
+                                                            <dd style="display: inline-block;margin-left: 37px;"> <?=$price?></dd>
+                                                         </dl>
+                                                         <dl class="pe-3">
+                                                            <dt style="display: inline-block;">Buy 1:</dt>
+                                                            <dd style="display: inline-block;margin-left: 37px;"> 2 Mil</dd>
+                                                         </dl>
+                                                         <dl class="pe-3">
+                                                            <dt style="display: inline-block;">Buy 1:</dt>
+                                                            <dd style="display: inline-block;margin-left: 37px;"> 2 Mil</dd>
+                                                         </dl>
+                                                         <dl class="pe-3">
+                                                            <dt style="display: inline-block;">Buy 1:</dt>
+                                                            <dd style="display: inline-block;margin-left: 37px;"> 2 Mil</dd>
+                                                         </dl>
+                                                      </div>
+                                                      <div class="accordian-side-btn my-2">
+                                                         <div class="d-flex">
+                                                            <a href="<?php echo $product->add_to_cart_url() ?>" value="<?php echo esc_attr($product->get_id()); ?>" class="btn btn-primary px-4 border-0 product-button ms-5 ajax_add_to_cart add_to_cart_button" data-product_id="<?php echo get_the_ID(); ?>" data-product_sku="<?php echo esc_attr($sku) ?>" aria-label="Add “<?php the_title_attribute() ?>” to your cart">Add to cart</i></a>
+                                                            <?php
+                                                            //do_action('woocommerce_simple_add_to_cart');
+
+                                                            ?>
+                                                            <!-- <button class="btn btn-primary px-3 border-0 product-button1"
+                                                               type="submit">1</button>
+                                                            <button class="btn btn-primary   px-4 border-0 product-button ms-5"
+                                                               type="submit">Add to cart</button> -->
+                                                         </div>
+                                                      </div>
+                                                   </div>
+                                                </div>
+                                             </div>
+                                          </div>
+                                       </div>
+                                    </div>
+                                 </div>
+                              </div>
+                           <?php
+                           $newloopcount++;
+                           endwhile;
+                        endif;
+
+                     }
+
+
+                  /*if(!empty($updatedTerms)){
+                     $i++;
+                     echo 'index is=='.$i;
+                     echo '<pre>'; print_r($updatedTerms);
+                  }else{
+                     echo 'nothing found';
+                  }*/
+                  /*$product_child_terms =  get_terms(
                                         array(
                                             'taxonomy'   => $taxonomy->taxonomy,
                                             'parent' => $taxonomy->term_id,
@@ -274,7 +358,7 @@ $taxonomy_img = get_taxonomy_image( $taxonomy->term_taxonomy_id );
                         $i++;
                         endwhile;
                      endif;
-                  }
+                  }*/
                   ?>
                   <!-- <h3>Drum Liners on Rolls</h3>
 
