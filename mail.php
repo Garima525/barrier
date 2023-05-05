89b9154b00f73e827840a08651ee143da17248e8
Garima Chauhan <garima.chauhan@infostride.com>
2023-05-06 03:38:58 +0530

    testing the mail

--- mail.php
+++ mail.php
@@ -3,7 +3,7 @@ $to = "garima.chauhan@infostride.com";
 $subject = "Test Email";
 $message = "This is a test email.";
 $headers = "From: chauhangarima525@gmail.com" . "\r\n" .
-           "Reply-To: sender@example.com" . "\r\n" .
+           "Reply-To: chauhangarima525@gmail.com" . "\r\n" .
            "X-Mailer: PHP/" . phpversion();
 
 if (mail($to, $subject, $message, $headers)) {
