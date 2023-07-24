b97d307b655e8752d47a2d34d102f9aa2ed8cdf0
Garima Chauhan <garima.chauhan@infostride.com>
2023-07-25 01:24:46 +0530

    add the additional info over the category page

--- wp-content/themes/barrier/taxonomy-product_cat.php
+++ wp-content/themes/barrier/taxonomy-product_cat.php
@@ -231,7 +231,7 @@ if ($taxonomy_title != "BAGS") { ?>
                                     <td> <?= $finish ?> 
                                           <?php if ($material) { ?>  
                                           /<?= $material ?>
-                                          <?php } ?>
+                                          <?php } ?>/<?=$feature?>
                                     </td>
                                     <?php }} elseif($capacity) { ?>
                                     <td><?= $capacity ?></td>
